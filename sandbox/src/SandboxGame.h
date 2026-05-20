44dd3992a49baa10c7f73e5d8e9be29d728c1a9d
Erno Pakarinen <erpakari@gmail.com>
2026-05-20 11:15:42 +0300

    refactor: relocate Camera2D from sandbox to engine view subsystem
    - Moved Camera2D.h from sandbox/src/ to engine/include/thengine/view/.
    - Updated SandboxGame.h to include the new engine header path.

--- sandbox/src/SandboxGame.h
+++ sandbox/src/SandboxGame.h
@@ -3,7 +3,7 @@
 #include "thengine/ContentManager.h"
 #include "thengine/Game.h"
 #include "thengine/graphics/BasicEffect.h"
-#include "Camera2D.h"
+#include "thengine/view/Camera2D.h"
 #include "thengine/graphics/Sprite.h"
 #include "thengine/graphics/Texture.h"
 #include "thengine/graphics/SpriteBatch.h"
